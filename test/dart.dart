ccee4e6aa4cc9d1e967589bbeb4003027226cd63
Jakub Jodełka <jakub.jodelka@10clouds.com>
2019-12-04 16:53:24 +0100

    generators test

--- test/dart.dart
+++ test/dart.dart
@@ -3,3 +3,6 @@ import 'dart:io';
 import 'package:{{project.package}}/data/project_info.dart';
 import 'package:{{project.package}}/util/logger.dart';
 
+print({{extra.q1}});
+print({{extra.q2}});
+
